7c2a4bc5ac5e03a8140ce1e73b31670bbdbbb1d2
Lakei87 <monastyrskyis@gmail.com>
2024-02-25 08:02:29 +0100

    add mobile mobile navigation

--- js/menu.js
+++ js/menu.js
@@ -7,5 +7,19 @@
     menuBtnRef.classList.toggle("is-open");
     menuBtnRef.setAttribute("aria-expanded", !expanded);
     mobileMenuRef.classList.toggle("is-open");
+
+  });
+  
+  const navAboutUs = document.getElementById("header-nav__link-aboutUs");
+  navAboutUs.addEventListener("click", () => {
+    mobileMenuRef.classList.toggle("is-open")
+  });
+  const navProducts = document.getElementById("header-nav__link-products");
+  navProducts.addEventListener("click", () => {
+    mobileMenuRef.classList.toggle("is-open")
+  });
+  const navContacts = document.getElementById("header-nav__link-contacts");
+  navContacts.addEventListener("click", () => {
+    mobileMenuRef.classList.toggle("is-open")
   });
 })();
