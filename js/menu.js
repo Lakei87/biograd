116edba33ead18979f74473308b1671067cb9618
Lakei87 <monastyrskyis@gmail.com>
2024-02-25 09:28:14 +0100

    some changes in page header menu

--- js/menu.js
+++ js/menu.js
@@ -1,25 +1,27 @@
 (() => {
   const menuBtnRef = document.querySelector("[data-menu-button]");
   const mobileMenuRef = document.querySelector("[data-menu]");
+
   menuBtnRef.addEventListener("click", () => {
-    const expanded =
-      menuBtnRef.getAttribute("aria-expanded") === "true" || false;
+    const expanded = menuBtnRef.getAttribute("aria-expanded") === "true" || false;
     menuBtnRef.classList.toggle("is-open");
     menuBtnRef.setAttribute("aria-expanded", !expanded);
     mobileMenuRef.classList.toggle("is-open");
-
   });
   
   const navAboutUs = document.getElementById("header-nav__link-aboutUs");
   navAboutUs.addEventListener("click", () => {
+    menuBtnRef.classList.toggle("is-open");
     mobileMenuRef.classList.toggle("is-open")
   });
   const navProducts = document.getElementById("header-nav__link-products");
   navProducts.addEventListener("click", () => {
+    menuBtnRef.classList.toggle("is-open");
     mobileMenuRef.classList.toggle("is-open")
   });
   const navContacts = document.getElementById("header-nav__link-contacts");
   navContacts.addEventListener("click", () => {
+    menuBtnRef.classList.toggle("is-open");
     mobileMenuRef.classList.toggle("is-open")
   });
 })();
